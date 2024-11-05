f3080cd30781ef98fbddc4d2fedba21f41ba072a
Justin-Wang2-ICD201-01 <justin.wang@student.tdsb.on.ca>
2024-11-04 21:46:46 -0500

    Update PP1_9.py

--- PP1_9.py
+++ PP1_9.py
@@ -1,31 +1,31 @@
 
 
 def q1(): 
-  #Write Assignment code here
-
+  print('"Hello World"')
 
 def q2(): 
-  #Write Assignment code here
-
+  text = input("Input a word: ")
+  print(text.lower())
+  print(text.upper())
 
 def q3(): 
-  #Write Assignment code here
-
-
+  word = input("Input a word that is at least 5 letters long: ")
+  print(word[1:4])
 def q4(): 
-  #Write Assignment code here
-
+  text = input("Input a word: ")
+  print(text.index("o"))
 
 def q5(): 
-  #Write Assignment code here
+  text = input("Input a word: ")
+  print(len(text))
 
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+#q1()
+#q2()
+#q3()
+#q4()
+#q5()
